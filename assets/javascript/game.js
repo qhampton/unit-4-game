689aa6c2100c3a0c57e41e9e737d40512ded0b76
Quinlan Hampton <qnhampton93@gmail.com>
2019-07-15 21:58:42 -0700

    adding commented html, css, and js files, potential images to use in the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// html variable to be manipulated
+//global variables
+
+//functions to use throughtout the game
+//make a function that creates character "buttons"
+
+//calling a character to the choice section
+
+//clicking ability
+
+//selecting one character and moving the others to enemy section section-use this to be able to be what the user selects
+
+//select an enemy and put in defender section 
+
+//attacking button - user attacks defender and then check if health is zero, if defender is not zero show current value, if zero then remove enemy and increase score
+
+//function restart game
+
